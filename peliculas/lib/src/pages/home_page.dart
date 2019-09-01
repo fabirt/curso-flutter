d1d26746cc9eae0662c6b2bbec7baec51fceae9e
Fabian Diartt <fdiartt@technisupport.com>
2019-08-31 20:04:50 -0500

    Creando widget CardSwiper

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('hola mundo'),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Películas en cine'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
       )
     );
   }
+
+
+  Widget _swiperTarjetas() {
+    return CardSwiper(
+      peliculas: [1, 2, 3, 4, 5],
+    );    
+  }
+
 }
