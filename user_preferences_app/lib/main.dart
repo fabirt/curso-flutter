d2f1281e6d7a7c1b7691e6c784c0573beefd010f
Fabian Diartt <fdiartt@technisupport.com>
2019-09-08 08:09:10 -0500

    Inicio de proyecto preferencias de usuario

---
+++ user_preferences_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:user_preferences_app/src/pages/home_page.dart';
+import 'package:user_preferences_app/src/pages/settings_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Preferencias',
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName    : ( BuildContext context ) => HomePage(),
+        SettingsPage.routeName: ( BuildContext context ) => SettingsPage()
+      },
+    );
+  }
+}
