9231a31c819454ae4cbc2af09ee8971dcdfa0458
Fabian Diartt <fdiartt@technisupport.com>
2019-09-13 09:34:07 -0500

    Diseño de fondo de pantalla del login

--- form_validation/lib/main.dart
+++ form_validation/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:form_validation/src/pages/home_page.dart';
 import 'package:form_validation/src/pages/login_page.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarBrightness: Brightness.dark
+    ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Form Validation',
