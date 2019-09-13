be37d856bb10427cd235cefaac8ebfde122de595
Fabian Diartt <fdiartt@technisupport.com>
2019-09-13 15:04:00 -0500

    Logica para persistir los datos del Bloc

--- form_validation/lib/src/bloc/provider.dart
+++ form_validation/lib/src/bloc/provider.dart
@@ -5,10 +5,20 @@ export 'package:form_validation/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
 
+  static Provider _instance;
+
   final loginBloc = LoginBloc();
 
-  Provider({ Key key, Widget child })
+  factory Provider({ Key key, Widget child }) {
+    if (_instance == null) {
+      _instance = new Provider._internal(key: key, child: child);
+    }
+    return _instance;
+  }
+
+  Provider._internal({ Key key, Widget child })
     : super(key: key, child: child);
+
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
