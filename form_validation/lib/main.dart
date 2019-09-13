e456d3e2ca62d309c7318f0241bb3c73a1b00af2
Fabian Diartt <fdiartt@technisupport.com>
2019-09-13 09:11:51 -0500

    Inicio de proyecto validación de formulario

---
+++ form_validation/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:form_validation/src/pages/home_page.dart';
+import 'package:form_validation/src/pages/login_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Form Validation',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage()
+      },
+    );
+  }
+}
