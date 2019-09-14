5e7ac63cc5b348405e01cd7222c9d7913c251110
Fabian Diartt <fdiartt@technisupport.com>
2019-09-14 15:07:20 -0500

    Creando modelo producto

---
+++ form_validation/lib/src/models/product_model.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+ProductModel productModelFromJson(String str) => ProductModel.fromJson(json.decode(str));
+
+String productModelToJson(ProductModel data) => json.encode(data.toJson());
+
+class ProductModel {
+    String id;
+    String title;
+    double price;
+    bool available;
+    String photoUrl;
+
+    ProductModel({
+        this.id,
+        this.title = '',
+        this.price = 0.0,
+        this.available = true,
+        this.photoUrl,
+    });
+
+    factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id        : json["id"],
+        title     : json["title"],
+        price     : json["price"],
+        available : json["available"],
+        photoUrl  : json["photoUrl"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id"        : id,
+        "title"     : title,
+        "price"     : price,
+        "available" : available,
+        "photoUrl"  : photoUrl,
+    };
+}
