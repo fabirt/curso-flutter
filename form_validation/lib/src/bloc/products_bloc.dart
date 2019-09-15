f870afb46e3d1dd149bf61b03f45c05af2696a79
Fabian Diartt <fdiartt@technisupport.com>
2019-09-15 09:04:59 -0500

    Creando Bloc para los productos

---
+++ form_validation/lib/src/bloc/products_bloc.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:rxdart/rxdart.dart';
+
+import 'package:form_validation/src/providers/products_provider.dart';
+import 'package:form_validation/src/models/product_model.dart';
+
+class ProductsBloc {
+  
+  final _productsController = new BehaviorSubject<List<ProductModel>>();  
+  final _loadingController = new BehaviorSubject<bool>();
+  final _productsProvider = new ProductsProvider();
+
+  Stream<List<ProductModel>> get productsStream => _productsController.stream;
+  Stream<bool> get loading => _loadingController.stream;
+
+  void getProducts() async {
+    final products = await _productsProvider.getProducts();
+    _productsController.sink.add(products);
+  }
+
+  void createProduct(ProductModel product) async {
+    _loadingController.sink.add(true);
+    await _productsProvider.createProduct(product);
+    _loadingController.sink.add(false);
+  }
+  
+  void updateProduct(ProductModel product) async {
+    _loadingController.sink.add(true);
+    await _productsProvider.updateProduct(product);
+    _loadingController.sink.add(false);
+  }
+  
+  void deleteProduct(String id) async {
+    await _productsProvider.deleteProduct(id);
+  }
+
+  Future<String> uploadImage(File image) async {
+    _loadingController.sink.add(true);
+    final imageUrl = await _productsProvider.uploadImage(image);
+    _loadingController.sink.add(false);
+    return imageUrl;
+  }
+
+  dispose() {
+    _productsController?.close();
+    _loadingController?.close();
+  }  
+}
